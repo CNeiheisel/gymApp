807da3bf382e6855c7cdc4c7aa9e71d1a65ee97d
CNeiheisel <124808780+CNeiheisel@users.noreply.github.com>
2025-09-14 15:33:07 -0400

    Update settings.py

--- gymapp/settings.py
+++ gymapp/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# Directory where collectstatic will put all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
