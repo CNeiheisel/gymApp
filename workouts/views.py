31966b2c789c4203a29905d151c0710a65d05cb0
CNeiheisel <124808780+CNeiheisel@users.noreply.github.com>
2025-09-14 18:03:37 -0400

    Update views.py

--- workouts/views.py
+++ workouts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import WorkoutTemplate, WorkoutEntry, Workout
+from .models import WorkoutTemplate, WorkoutEntry, LoggedExercise
 
 # Home page
 def home(request):
