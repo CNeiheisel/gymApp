7166846b28aa9359ebe305e5202f200424fe1f4e
CNeiheisel <124808780+CNeiheisel@users.noreply.github.com>
2025-09-14 16:23:56 -0400

    Update urls.py

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,13 +1,14 @@
+from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
-from . import views
+from workouts import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('templates/', views.templates, name='templates'),
     path('log/<int:template_id>/', views.log_workout, name='log_workout'),
     path('history/', views.history, name='history'),
+    path('login/', auth_views.LoginView.as_view(template_name='workouts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-
 ]
