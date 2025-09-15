093c4e7fe7ad327b752760879d7bf86ea24c9b91
CNeiheisel <124808780+CNeiheisel@users.noreply.github.com>
2025-09-14 20:53:18 -0400

    Update urls.py

--- workouts/urls.py
+++ workouts/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='workouts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('register/', views.register, name='register'),  # Add registration page
+    path('templates/create/', views.create_template, name='create_template'),
+
 ]
