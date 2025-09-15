0ad7235e88d1937474cd26562c8781400c62d0b7
CNeiheisel <124808780+CNeiheisel@users.noreply.github.com>
2025-09-14 20:51:36 -0400

    Update forms.py

--- workouts/forms.py
+++ workouts/forms.py
@@ -1,1 +1,13 @@
+from django import forms
+from .models import WorkoutTemplate, TemplateExercise, Exercise
+
+class WorkoutTemplateForm(forms.ModelForm):
+    class Meta:
+        model = WorkoutTemplate
+        fields = ['name']
+
+class TemplateExerciseForm(forms.ModelForm):
+    class Meta:
+        model = TemplateExercise
+        fields = ['exercise', 'sets', 'reps']
 
